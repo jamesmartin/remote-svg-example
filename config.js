8e0056cb1a1aac9def6cc376a39e39021656f165
James Martin <james@roomz.com>
2015-08-26 20:44:28 +1000

    Use v0.1.1 of RemoteSvg to make things work in Safari

--- config.js
+++ config.js
@@ -11,10 +11,14 @@ System.config({
 System.config({
   "map": {
     "css": "github:systemjs/plugin-css@0.1.14",
-    "remote-svg": "github:jamesmartin/remote-svg@0.1.0",
+    "remote-svg": "github:jamesmartin/remote-svg@0.1.1",
     "traceur": "github:jmcriffey/bower-traceur@0.0.88",
     "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.88",
     "github:jamesmartin/remote-svg@0.1.0": {
+      "lie": "npm:lie@2.9.1",
+      "underscore": "npm:underscore@1.8.3"
+    },
+    "github:jamesmartin/remote-svg@0.1.1": {
       "lie": "npm:lie@2.9.1",
       "underscore": "npm:underscore@1.8.3"
     },
